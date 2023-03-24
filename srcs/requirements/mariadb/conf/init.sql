ec832fd5a473db1a241860d0064c03339dfa669d
Raphael Bourdillat <raphael.bourdillat@outlook.f>
2023-03-24 16:56:46 +0100

    fixed wp hostname in mariadb

--- srcs/requirements/mariadb/conf/init.sql
+++ srcs/requirements/mariadb/conf/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE wordpress;
-CREATE USER wordpress@wordpress IDENTIFIED BY 'qwerty';
+CREATE USER wordpress@wordpress.mynet IDENTIFIED BY 'qwerty';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@wordpress;
 FLUSH PRIVILEGES;
