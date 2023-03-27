b31aef99ff65f13aef2a599499aede0df67a918d
Raphael BOURDILLAT <rbourdil@e1r7p3.clusters.42paris.fr>
2023-03-27 16:32:46 +0200

    changed url to rbourdil.42.fr

--- srcs/requirements/wordpress/conf/docker-entrypoint.sh
+++ srcs/requirements/wordpress/conf/docker-entrypoint.sh
@@ -16,7 +16,7 @@ fi
 
 if ! sudo -u www-data wp core is-installed --path=/srv/www/wordpress; then
 
-	sudo -u www-data wp core install --url=localhost --title=rbourdil.42.fr --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWD} --admin_email="${WP_ADMIN_USER}@wp.fr" --path=/srv/www/wordpress
+	sudo -u www-data wp core install --url=rbourdil.42.fr --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWD} --admin_email="${WP_ADMIN_USER}@wp.fr" --path=/srv/www/wordpress
 	sudo -u www-data wp user create ${WP_COMMON_USER} "${WP_COMMON_USER}@wp.fr" --user_pass=${WP_COMMON_PASSWD} --path=/srv/www/wordpress
 
 fi
